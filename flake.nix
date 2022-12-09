16a09faf1bef2ecc6020572667eaf074b2d7cc4b
Astrid Yu <astrid@astrid.tech>
2022-12-09 13:25:11 -0800

    Package using nix

--- flake.nix
+++ flake.nix
@@ -1,14 +1,40 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
-    devShells.x86_64-linux.default = with pkgs; mkShell {
-      buildInputs = [
-        python3Packages.aiohttp
-        python3Packages.yarl
-      ];
-    };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = with pkgs;
+          stdenvNoCC.mkDerivation {
+            pname = "year-of-bot";
+            version = "0.1.0";
+
+            src = ./.;
+
+            runtimeInputs =
+              [ python python3Packages.aiohttp python3Packages.yarl ];
+
+            installPhase = ''
+              mkdir -p $out/bin
+              cp * $out/bin
+              chmod +x $out/bin/year-of-bot.py
+            '';
+
+            meta = {
+              description = "A Pleroma bot that makes technology predictions.";
+              mainProgram = "year-of-bot.py";
+            };
+          };
+
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [ python3Packages.aiohttp python3Packages.yarl ];
+          };
+      });
 }
