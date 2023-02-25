5bc5dadc1524708f5e452f7d2c0c66618832a000
Natty <natty.sh.git@gmail.com>
2023-02-25 23:16:55 +0100

    Add consistent sorting

--- sort-data.sh
+++ sort-data.sh
@@ -5,7 +5,7 @@ files=(subjects.txt predictions/*.txt)
 tmp=$(mktemp)
 
 for f in "${files[@]}"; do
-    sort -f -o "$f" "$f"
+    LC_ALL=C sort -f -o "$f" "$f"
     cp "$f" $tmp
     uniq $tmp > "$f"
 done
